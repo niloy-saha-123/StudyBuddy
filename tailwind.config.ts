661070226cdd71e0942b99e8753f851ff537947b
niloy-saha-123 <niloysaha2404@gmail.com>
2025-01-19 23:07:36 -0500

    tailwind stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: 'class', // Add this line to enable class-based dark mode
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        // You can add dark mode specific colors if needed
         'background-dark': "var(--background-dark)",
         'foreground-dark': "var(--foreground-dark)",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+}
+
+export default config
